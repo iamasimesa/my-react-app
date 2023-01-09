c0e9b9b5221147e7bd8d0a2b92b99338567b1c62
Asim Esa <intelligentzaram@gmail.com>
2023-01-09 18:57:04 +0400

    Make Counter App useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className="App">
-      <Person name={'React'} lastName={'JS'} age={'13'}/>
-      <Person name={'Angular'} lastName={'JS'} age={'18'}/>
-      <Person name='Laravel' lastName='PHP' age= {6*2} />
+      
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>-</button>
       </div>
      
   );
