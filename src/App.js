1834d9595cf6f426210ab10f67f64d79cb94caf4
Asim Esa <intelligentzaram@gmail.com>
2023-01-18 12:58:57 +0400

    Build Movie Search App via API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
+const API_URL = "http://www.omdbapi.com?apikey=ab2ac1ba";
 
-
+const movie1 = {
+  Title: "Spiderman the Verse",
+  Year: "2019–",
+  imdbID: "tt12122034",
+  Type: "series",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg",
+};
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
   return (
-    <div className="App">
-      
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    <div className="app">
+      <h1>Movie Island</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
-     
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
